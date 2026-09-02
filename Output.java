1b131d78814839c250fa45916ed57d601c0f94f9
Sachi Singh <sachisingh1108@gmail.com>
2026-09-02 21:36:05 +0530

    hello

---
+++ Output.java
@@ -0,0 +1,8 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("ID: 106");
+        System.out.println("Name: Ram");
+        System.out.println("Age: 21");
+        System.out.println("--------------------");
+    }
+}
